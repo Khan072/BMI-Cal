9bd42ac5fb8ebbd0047941219abedd00b1388f54
Khan072 <mdaabidkhan28@gmail.com>
2024-09-24 23:30:48 +0530

    first commit

--- assoarray.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$associativeArray = ["name" => "John", "age" => 30, "email" => "rohn@example.com"];
-foreach ($associativeArray as $key => $value) {
-    echo "$key: $value" ;
-    echo "<br>";
-}
-
-?>
